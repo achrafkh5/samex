806295248cb2607562fe699f3d2f16f83dc4b441
achraf <acherf2011@gmail.com>
2025-10-29 04:14:59 +0100

    fixed home page and insc docs

--- app/cars/page.js
+++ app/cars/page.js
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import CarsPageContent from '../components/CarsPageContent';
@@ -11,7 +12,16 @@ export default function CarsPage() {
   return (
       <div className="min-h-screen bg-white dark:bg-gray-900">
         <Navbar />
-        <CarsPageContent />
+        <Suspense fallback={
+          <div className="flex items-center justify-center min-h-screen">
+            <div className="text-center">
+              <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto mb-4"></div>
+              <p className="text-gray-600 dark:text-gray-400">Loading cars...</p>
+            </div>
+          </div>
+        }>
+          <CarsPageContent />
+        </Suspense>
         <Footer />
       </div>
   );
